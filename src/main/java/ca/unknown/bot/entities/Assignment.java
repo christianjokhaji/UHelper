5c834a0939ac7c0fcf6a0686ab30f550b9c2b830
aria <aria.coventry@mail.utoronto.ca>
2024-07-06 16:05:51 -0400

    Added very basic javadoc for the Assignment class

--- src/main/java/ca/unknown/bot/entities/Assignment.java
+++ src/main/java/ca/unknown/bot/entities/Assignment.java
@@ -1,6 +1,12 @@
 package ca.unknown.bot.entities;
 import java.util.Calendar;
 
+/**
+ * Stores the user's assignment details.
+ * @param classCode The code of the class which the assignment is from.
+ * @param assignmentName The name of the assignment.
+ * @param dueDate The date and time that the assignment is due.
+ */
 public class Assignment {
     private String classCode;
     private String assignmentName;
@@ -12,18 +18,34 @@ public class Assignment {
         this.dueDate = dueDate;
     }
 
+    /**
+     * Return the course code of the assignment.
+     * @return
+     */
     public String getClassCode(){
         return classCode;
     }
 
+    /**
+     * Return the name of the assignment.
+     * @return
+     */
     public String getAssignmentName(){
         return assignmentName;
     }
 
+    /**
+     * Return the due date of the assignment.
+     * @return
+     */
     public Calendar getDueDate(){
         return dueDate;
     }
 
+    /**
+     * Print a reminder statement for when the assignment is due (incl. date and time).
+     * @return
+     */
     public String toString(){
         return "Your assignment, '" + assignmentName + "', for class " + classCode +
                 " is due on " + dueDate.getTime();
