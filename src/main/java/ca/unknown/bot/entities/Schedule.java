5212ae0fef18ac9613cb4848e45d4879fe6c4875
aria <aria.coventry@mail.utoronto.ca>
2024-07-15 03:31:25 -0400

    Created a Schedule entity which will hold a user's scheduled events (sortable by date)

--- src/main/java/ca/unknown/bot/entities/Schedule.java
+++ src/main/java/ca/unknown/bot/entities/Schedule.java
@@ -1,5 +1,45 @@
 package ca.unknown.bot.entities;
+import java.util.*;
 
 public class Schedule {
-    
+
+    private List<ScheduledEvent> events = new ArrayList<>();
+
+    private String user;
+
+    public Schedule(String user){
+        this.user = user;
+    }
+
+    public void addEvent(Exam e){
+        events.add(e);
+    }
+
+    public void addEvent(Assignment a){
+        events.add(a);
+    }
+
+    public void addEvent(ScheduledEvent s){
+        events.add(s);
+    }
+
+    public void sort(){
+        Collections.sort(events);
+    }
+
+    public String getUser(){
+        return user;
+    }
+
+    public void printSchedule(){
+        sort();
+        System.out.println("Here is your upcoming schedule: \n");
+        for(ScheduledEvent s: events){
+            System.out.println(s.toString() + "\n");
+        }
+    }
+
+    public void clearSchedule(){
+        events.clear();
+    }
 }
