37cbb8697d5ad628ac18e6a1e3da7c25b119dcda
aria <aria.coventry@mail.utoronto.ca>
2024-07-17 02:23:48 -0400

    edited so that this use case interactor calls subordinate interactors to perform the tasks of schedudling events. this will primarily be used to send scheduled reminders which is more difficult

---
+++ src/main/java/ca/unknown/bot/use_cases/schedule_reminder/ScheduledReminderInteractor.java
@@ -0,0 +1,60 @@
+package ca.unknown.bot.use_cases.schedule_reminder;
+
+import ca.unknown.bot.data_access.schedule_reminder.ScheduledReminderDAO;
+import ca.unknown.bot.data_access.schedule_reminder.ScheduledReminderDataAccessInterface;
+import ca.unknown.bot.entities.schedule_reminder.*;
+import ca.unknown.bot.interface_interactor.schedule_reminder.ScheduledReminderController;
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class ScheduledReminderInteractor extends ListenerAdapter {
+    /**
+     *
+     *
+     * @param event represents a SlashCommandInteraction event.
+     */
+    ScheduledReminderDataAccessInterface scheduleDAO = new ScheduledReminderDAO();
+    ScheduleFactory scheduleFactory = new UserScheduleFactory();
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+
+        if (event.getName().equals("schedule_event")) {
+            String user = event.getInteraction().getUser().getName();
+            if(!scheduleDAO.existsByUser(user)){
+                scheduleFactory.create(user);
+            }
+
+            ScheduledReminderController scheduledReminderController = new ScheduledReminderController(event);
+            ScheduledReminderInputData scheduledReminderInputData = scheduledReminderController.getScheduledReminderInputData();
+            new ScheduledEventInteractor(scheduleDAO).execute(scheduledReminderInputData, user);
+
+            event.reply(scheduleDAO.getSchedule(user).toString());
+        }
+        else if (event.getName().equals("schedule_exam")) {
+            String user = event.getInteraction().getUser().getName();
+            if(!scheduleDAO.existsByUser(user)){
+                scheduleFactory.create(user);
+            }
+
+            ScheduledReminderController scheduledReminderController = new ScheduledReminderController(event);
+            ScheduledReminderInputData scheduledReminderInputData = scheduledReminderController.getScheduledReminderInputData();
+            new ScheduledExamInteractor(scheduleDAO).execute(scheduledReminderInputData, user);
+
+            event.reply(scheduleDAO.getSchedule(user).toString());
+        }
+        else if (event.getName().equals("schedule_assignment")){
+            String user = event.getInteraction().getUser().getName();
+            if(!scheduleDAO.existsByUser(user)){
+                scheduleFactory.create(user);
+            }
+
+            ScheduledReminderController scheduledReminderController = new ScheduledReminderController(event);
+            ScheduledReminderInputData scheduledReminderInputData = scheduledReminderController.getScheduledReminderInputData();
+            new ScheduledAssignmentInteractor(scheduleDAO).execute(scheduledReminderInputData, user);
+
+            event.reply(scheduleDAO.getSchedule(user).toString());
+        }
+    }
+
+}
