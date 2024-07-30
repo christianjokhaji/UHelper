f6e0397cc4cfcd6899d78d1774a4bd21606fa76c
Christian <71178988+christianjokhaji@users.noreply.github.com>
2024-07-30 12:50:56 -0400

    Created GUI Component Tree using IntelliJ Swing UI Designer.

---
+++ src/main/java/ca/unknown/bot/gui/UHelper.java
@@ -0,0 +1,20 @@
+package ca.unknown.bot.gui;
+
+import javax.swing.*;
+
+public class UHelper extends JFrame {
+    private JPanel main;
+    private JButton start;
+    private JTextField discordAPI;
+    private JTextField mealPrepID;
+    private JTextField mealPrepKey;
+    private JPanel auth;
+    private JPanel submit;
+    private JPanel data;
+    private JPanel discord;
+    private JPanel id;
+    private JPanel key;
+    private JLabel mealPrepKeyLabel;
+    private JLabel mealPrepIDLabel;
+    private JLabel discordLabel;
+}
