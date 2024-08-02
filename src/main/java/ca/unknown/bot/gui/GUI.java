826c4b3077e936cd1a5bc6c4c4aacfe55eb08e5e
Christian <71178988+christianjokhaji@users.noreply.github.com>
2024-08-02 12:56:22 -0400

    Added clickable hyperlinks for Discord Dev Portal and Edamam Recipe Search API.

--- src/main/java/ca/unknown/bot/gui/GUI.java
+++ src/main/java/ca/unknown/bot/gui/GUI.java
@@ -3,6 +3,13 @@ package ca.unknown.bot.gui;
 import ca.unknown.bot.app.Main;
 
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Objects;
 
 public class GUI extends JFrame {
     private final JPanel main = new JPanel();
@@ -12,8 +19,10 @@ public class GUI extends JFrame {
     private final JPanel start = new JPanel();
     private final JLabel discordAPIToken = new JLabel("Discord API Token:");
     private final JTextField token = new JTextField(20);
+    private final JLabel devLink = new JLabel("Discord Dev Portal");
     private final JLabel mealPrepIDText = new JLabel("Meal Prep ID:");
     private final JTextField id =  new JTextField(20);
+    private final JLabel edamamLink = new JLabel("Edamam Recipe Search API");
     private final JLabel mealPrepKeyText = new JLabel("Meal Prep Key:");
     private final JTextField key = new JTextField(20);
 
@@ -33,16 +42,27 @@ public class GUI extends JFrame {
         main.add(mealPrepKey);
         main.add(start);
 
+        // Set up FlowLayout for each subsection.
+        discordAPI.setLayout(new FlowLayout(FlowLayout.LEFT));
+        mealPrepID.setLayout(new FlowLayout(FlowLayout.LEFT));
+        mealPrepKey.setLayout(new FlowLayout(FlowLayout.LEFT));
+
         // Set labels for text fields.
         discordAPIToken.setLabelFor(token);
         mealPrepIDText.setLabelFor(id);
         mealPrepKeyText.setLabelFor(key);
 
+        // Create hyperlinks.
+        devLink.setForeground(Color.BLUE.darker());
+        edamamLink.setForeground(Color.BLUE.darker());
+
         // Add components to each subsection.
         discordAPI.add(discordAPIToken);
         discordAPI.add(token);
+        discordAPI.add(devLink);
         mealPrepID.add(mealPrepIDText);
         mealPrepID.add(id);
+        mealPrepID.add(edamamLink);
         mealPrepKey.add(mealPrepKeyText);
         mealPrepKey.add(key);
 
@@ -60,7 +80,7 @@ public class GUI extends JFrame {
             System.setProperty("EDAMAM_KEY", keyText);
 
             // Start/stop the app.
-            if (startButton.getText() == "Stop UHelper") {
+            if (Objects.equals(startButton.getText(), "Stop UHelper")) {
                 // Stop
                 System.exit(0);
             }
@@ -71,6 +91,52 @@ public class GUI extends JFrame {
                 startButton.setText("Stop UHelper");
             }
         });
+
+        // Add hyperlink functionality.
+        devLink.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                try {
+                    Desktop.getDesktop().browse(new URI("https://discord.com/developers/applications"));
+                } catch (IOException | URISyntaxException e1) {
+                    e1.printStackTrace();
+                }
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                devLink.setText("<html><a href='https://discord.com/developers/applications'>" +
+                        "Discord Dev Portal</a></html>");
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                devLink.setText("Discord Dev Portal");
+            }
+        });
+
+        edamamLink.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                try {
+                    Desktop.getDesktop().browse(new URI("https://developer.edamam.com/edamam-recipe-api"));
+                } catch (IOException | URISyntaxException e1) {
+                    e1.printStackTrace();
+                }
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                edamamLink.setText("<html><a href='https://developer.edamam.com/edamam-recipe-api'>" +
+                        "Edamam Recipe Search API</a></html>");
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                edamamLink.setText("Edamam Recipe Search API");
+            }
+        });
+
         start.add(startButton);
     }
 
